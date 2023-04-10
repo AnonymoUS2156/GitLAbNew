d447c7aa754df05c4a7c954d2bfb3405793d0812
Никита Грибков <nikgri@list.ru>
2023-04-10 13:58:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 4;
+            Console.WriteLine($"5*Math.Cos(2*{s})={5*Math.Cos(2*s)}");
+            Console.ReadKey();
+        }
+    }
+}
